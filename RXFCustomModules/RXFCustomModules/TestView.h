70f492fefd4f4beadfd4807562b4be992dbc86a7
admin <admin@admindeiMac.local>
2017-08-08 15:08:10 +0800

    add View

---
+++ RXFCustomModules/RXFCustomModules/TestView.h
@@ -0,0 +1,13 @@
+//
+//  TestView.h
+//  RXFCustomModules
+//
+//  Created by admin on 2017/8/8.
+//  Copyright © 2017年 renxiaofeng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TestView : UIView
+
+@end
---
+++ RXFCustomModules/RXFCustomModules/TestView.m
@@ -0,0 +1,21 @@
+//
+//  TestView.m
+//  RXFCustomModules
+//
+//  Created by admin on 2017/8/8.
+//  Copyright © 2017年 renxiaofeng. All rights reserved.
+//
+
+#import "TestView.h"
+
+@implementation TestView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
